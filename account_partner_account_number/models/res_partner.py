2d799eaf8c7e374a059135d8cd2b5ad484d55ebe
clementthomas <clement@cotyl.net>
2023-06-09 09:44:46 +0200

    [IMP] account_partner_account_code : changes on field account_number rename field account_number to account_code field is now unique

--- account_partner_account_number/models/res_partner.py
+++ account_partner_account_number/models/res_partner.py
@@ -4,4 +4,10 @@ from odoo import fields, models, _
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    account_number = fields.Char('Account number')
+    account_code = fields.Char('Account code')
+
+    _sql_constraints = [
+        ('account_coder_unique', 
+        'unique(account_code)',
+        'Choose another value of account code - it has to be unique!')
+    ]
