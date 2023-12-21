b03af83a9aa963a92c9984976296f845bd36854e
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2023-12-21 16:23:16 +0100

    [ADD]14.0-account_usability:reconcile search by amount

---
+++ account_usability/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Account Usability Elabore',
+    'version': '16.0.1.0.0',
+    'description': 'account usability Elabore : improve account usability in v16',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'AGPL-3',
+    'category': '',
+    'depends': [
+        'account','base','account_reconcile_oca'
+    ],
+    'data': [
+        'views/account_search.xml',
+        'views/account_bank_statement_line_reconcile_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
