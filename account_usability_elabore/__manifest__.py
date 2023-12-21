aba8b8669acbfd59d47f7d28f00ac0fba45d31ea
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2023-12-21 16:47:07 +0100

    [ADD] account_usability_elabore: create add-on
    [CLN] account_usability_elabore: rename add-on

---
+++ account_usability_elabore/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Account Usability Elabore',
+    'version': '16.0.1.0.0',
+    'description': 'account usability Elabore : improve account usability in v16',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'AGPL-3',
+    'category': '',
+    'depends': [
+        'account','base','account_reconcile_oca'
+    ],
+    'data': [
+        'views/account_search.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
