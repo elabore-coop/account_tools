c521801cb3c9a1714152367656de1301ff92ad17
Stéphan Sainléger <contact@stephansainleger.fr>
2024-08-02 10:32:47 +0200

    [MIG] migrate add-on account_quotation_sale_order_invoice_title

---
+++ account_quotation_sale_order_invoice_title/wizard/sale_make_invoice_advance.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    def _prepare_invoice_values(self, order, name, amount, so_line):
+        res = super(SaleAdvancePaymentInv, self)._prepare_invoice_values(
+            order, name, amount, so_line
+        )
+        res["move_title"] = order.so_title
+        return res
