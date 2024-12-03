d6d39d536d5b97c9ea712a28b9d6cf8100b98a34
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-12-03 15:04:25 +0100

    [FIX]account_move_protect_deleting:manage recordset

--- account_move_protect_deleting/models/account_move.py
+++ account_move_protect_deleting/models/account_move.py
@@ -11,25 +11,25 @@ class AccountMove(models.Model):
                         when the check deposit is reset to draft.
                         This work the same with Cash deposit
         """
-
+        for rec in self:
         # search in account.cash.deposit if account move is this one
-        for cash_deposit in self.env['account.cash.deposit'].search([]):
-            if cash_deposit.move_id == self:
-                print (cash_deposit.move_id, self)
-                is_cash_deposit = True
+            for cash_deposit in rec.env['account.cash.deposit'].search([]):
+                if cash_deposit.move_id == rec:
+                    print (cash_deposit.move_id, rec)
+                    is_cash_deposit = True
 
-        # search in account.check.deposit if account move is this one
-        for check_deposit in self.env['account.check.deposit'].search([]):
-            if check_deposit.move_id == self:
-                is_check_deposit = True
+            # search in account.check.deposit if account move is this one
+            for check_deposit in rec.env['account.check.deposit'].search([]):
+                if check_deposit.move_id == rec:
+                    is_check_deposit = True
 
-        if (
-            self.posted_before and
-                (
-                not is_cash_deposit
-                and not is_check_deposit
-                )
-            ):
-            raise UserError(_(""
-                "You cannot delete this account move because it has been posted."
-            ))
+            if (
+                rec.posted_before and
+                    (
+                    not is_cash_deposit
+                    and not is_check_deposit
+                    )
+                ):
+                raise UserError(_(""
+                    "You cannot delete this account move because it has been posted."
+                ))
